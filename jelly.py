cb484bfdc8daa9478a4ab48186112b2667fb150f
CAKEbuilder <tecuserx@gmail.com>
2016-09-18 12:56:39 -0400

    Update jelly.py

--- jelly.py
+++ jelly.py
@@ -8,6 +8,12 @@ cam = SimpleCV.Camera()
 
 normaldisplay = True
 
+# define counters for stuff you want to track
+jellycount = 0
+applecount = 0
+floorcount = 0
+
+
 
 while display.isNotDone():
 
@@ -16,8 +22,10 @@ while display.isNotDone():
                 normaldisplay = not(normaldisplay)
                 print "Display Mode:", "Normal" if normaldisplay else "Segmented"
 
-        img = cam.getImage().flipHorizontal()
+        #img = cam.getImage().flipHorizontal()
         #img = cam.getImage()
+        #img = cam.getImage().flipVertical()
+        img = cam.getImage().rotate90()
 
         # red blob detection (jelly/apples)
         dist = img.colorDistance(SimpleCV.Color.RED).dilate(2).invert()
@@ -26,7 +34,8 @@ while display.isNotDone():
 
         # green blob detection (floor)
         dist_g = img.colorDistance(SimpleCV.Color.GREEN).dilate(2).invert()
-        segmented_g = dist_g.stretch(150,255)
+        segmented_g = dist_g.stretch(130,255)
+
         blobs_g = segmented_g.findBlobs()
 
 
@@ -46,7 +55,6 @@ while display.isNotDone():
         img.applyLayers()
 
 
-
         # if red blobs are detected
         if blobs:
                 # check each blob
@@ -54,41 +62,57 @@ while display.isNotDone():
 
                         # look for jelly
                         # in a 640x480 frame with the S5 touching the top and bottom of the window, the jelly should have an estimated area of 450 (no math done for this, just trial and error)
-                        if abs(b.area() > 300):
+                        if abs(b.area() > 550):
                                 jelly = b
-                                print "JELLYYYYYYYYYYYYYYYYYY"
+
+                                # count the number of jellys so we can print them to the console
+                                #jellycount = len(blobs)
+                                #print "I see the jelly"
+
                                 #print b.area()
                                 center_point = jelly.centroid()
                                 jellybox_dim = (jelly.width(), jelly.height())
-                                jellybox = jellylayer.centeredRectangle(center_point, jellybox_dim)
+                                jellybox = jellylayer.centeredRectangle(center_point, jellybox_dim, color=(SimpleCV.Color.RED), width=2)
+
+
 
                         # look for apples
-                        if abs(b.area() < 200):
+                        if abs(b.area() < 200) and abs(b.area() >100):
                                 apple = b
-                                print "APPLESSSSSSSSSSSSSSSSS"
+
+                                #applecount = len(blobs)
+                                #print "I see an apple"
+
                                 center_point = apple.centroid()
                                 applebox_dim = (apple.width(), apple.height())
                                 applebox = applelayer.circle(center_point, apple.radius())
+                        else:
+                                applecount = 0
 
                 #print blobs
                 #print jelly.coordinates()
-        else:
-                print "no jelly"
+
+
+
 
         # if green blobs are detected
         if blobs_g:
                 for b in blobs_g:
-                        if abs(b.area() < 300):
+                        # look for floors
+                        # floors that touch the left/right edges of the screen aren't detected. I believe this is due to the fact that the floor and the phone are grouped into one blob
+                        if abs(b.area() < 800) and abs(b.width() > 20):
                                 floor = b
-                                print "FLOOORRRSSSSSSSSSSSSS"
+
+                                #print b.area()
+                                #print b.width()
+                                #print "I see floors"
+
                                 center_point = floor.centroid()
                                 floorbox_dim = (floor.width(), floor.height())
-                                floorbox = floorlayer.centeredRectangle(center_point, floorbox_dim)
-
-
-
-
-
+                                #floorbox = floorlayer.centeredRectangle(center_point, floorbox_dim)
+                                floorbox = floorlayer.centeredRectangle(center_point, floorbox_dim, color=(SimpleCV.Color.GREEN), width=2)
+                        else:
+                                floorcount = 0
 
         if normaldisplay:
                 img.show()
